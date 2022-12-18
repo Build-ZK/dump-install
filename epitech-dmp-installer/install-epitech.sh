dfc8eafe0368d384ceaf74998d05112aec5c65de
Julian <zekyro.exe@gmail.com>
2022-12-18 02:32:55 +0100

    Update install-epitech.sh

--- epitech-dmp-installer/install-epitech.sh
+++ epitech-dmp-installer/install-epitech.sh
@@ -57,5 +57,7 @@ sudo cp .zshrc $HOME/.zshrc
 
 ### CODING-STYLE-CHECKER ###
 
+sudo apt install docker
+sudo apt install docker-compose
 sudo cp coding-style.sh /usr/bin/coding-style
 sudo chmod +x coding-style
